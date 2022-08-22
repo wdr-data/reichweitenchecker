0e7a7bb905e9f7892c65f666045ddfc2c0f02924
Jannes Höke <mail@jhoeke.de>
2022-08-22 12:30:13 +0200

    🚧 Testing eject

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
