bf892eaf3a04c2dce8282185adff06929f877856
Jannes Höke <mail@jhoeke.de>
2022-08-22 14:09:21 +0200

    ✨ Basic Leaflet setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import logo from './logo.svg';
-import styles from './App.module.scss';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import * as React from 'react'
+import { useState, useCallback } from 'react'
 
-function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styles from './App.module.scss'
+import VirtualizedAutocomplete from './VirtualizedAutocomplete'
+const OPTIONS = ['Chicago', 'New York', 'San Francisco', 'Cologne'].sort()
+
+const COORDINATES = {
+  Chicago: [41.8781, -87.6298],
+  'New York': [40.7128, -74.0059],
+  'San Francisco': [37.7749, -122.4194],
+  Cologne: [50.9375, 6.96027]
 }
 
-export default App;
+function App () {
+  const [map, setMap] = useState(null)
+  const [selectedStop, setSelectedStop] = useState('Cologne')
+
+  const handleStopChange = useCallback(
+    (event, stop) => {
+      setSelectedStop(stop);
+      map.setView(COORDINATES[stop], 10)
+    },
+    [setSelectedStop, map]
+  );
+
+  return (
+    <div className={styles.App}>
+      <VirtualizedAutocomplete
+        options={OPTIONS}
+        label='Haltestelle'
+        onChange={handleStopChange}
+        defaultValue="Cologne"
+      />
+      <MapContainer
+        ref={setMap}
+        className={styles.MapContainer}
+        center={COORDINATES[selectedStop]}
+        zoom={10}
+        scrollWheelZoom={true}
+        touchZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {selectedStop && (
+          <Marker position={COORDINATES[selectedStop]} />
+        )}
+      </MapContainer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/VirtualizedAutocomplete.jsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import TextField from '@mui/material/TextField';
+import Autocomplete, { autocompleteClasses } from '@mui/material/Autocomplete';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import ListSubheader from '@mui/material/ListSubheader';
+import Popper from '@mui/material/Popper';
+import { useTheme, styled } from '@mui/material/styles';
+import { VariableSizeList } from 'react-window';
+import Typography from '@mui/material/Typography';
+
+const LISTBOX_PADDING = 8; // px
+
+function renderRow(props) {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  const inlineStyle = {
+    ...style,
+    top: style.top + LISTBOX_PADDING,
+  };
+
+  if (dataSet.hasOwnProperty('group')) {
+    return (
+      <ListSubheader key={dataSet.key} component="div" style={inlineStyle}>
+        {dataSet.group}
+      </ListSubheader>
+    );
+  }
+
+  return (
+    <Typography component="li" {...dataSet[0]} noWrap style={inlineStyle}>
+      {dataSet[1]}
+    </Typography>
+  );
+}
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef((props, ref) => {
+  const outerProps = React.useContext(OuterElementContext);
+  return <div ref={ref} {...props} {...outerProps} />;
+});
+
+function useResetCache(data) {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    if (ref.current != null) {
+      ref.current.resetAfterIndex(0, true);
+    }
+  }, [data]);
+  return ref;
+}
+
+// Adapter for react-window
+const ListboxComponent = React.forwardRef(function ListboxComponent(props, ref) {
+  const { children, ...other } = props;
+  const itemData = [];
+  children.forEach((item) => {
+    itemData.push(item);
+    itemData.push(...(item.children || []));
+  });
+
+  const theme = useTheme();
+  const smUp = useMediaQuery(theme.breakpoints.up('sm'), {
+    noSsr: true,
+  });
+
+  const itemCount = itemData.length;
+  const itemSize = smUp ? 36 : 48;
+
+  const getChildSize = (child) => {
+    if (child.hasOwnProperty('group')) {
+      return 48;
+    }
+
+    return itemSize;
+  };
+
+  const getHeight = () => {
+    if (itemCount > 8) {
+      return 8 * itemSize;
+    }
+    return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+  };
+
+  const gridRef = useResetCache(itemCount);
+
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <VariableSizeList
+          itemData={itemData}
+          height={getHeight() + 2 * LISTBOX_PADDING}
+          width="100%"
+          ref={gridRef}
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={(index) => getChildSize(itemData[index])}
+          overscanCount={5}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </VariableSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+    },
+  },
+});
+
+export default function VirtualizedAutocomplete({label, ...props}) {
+  return (
+    <Autocomplete
+      id="virtualize-demo"
+      sx={{ width: 300 }}
+      disableListWrap
+      PopperComponent={StyledPopper}
+      ListboxComponent={ListboxComponent}
+      groupBy={(option) => option[0].toUpperCase()}
+      renderInput={(params) => <TextField {...params} label={label} />}
+      renderOption={(props, option) => [props, option]}
+      renderGroup={(params) => params}
+      {...props}
+    />
+  );
+}
