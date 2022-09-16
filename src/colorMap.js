92579b1fb2bca27a25b6b1bbf16328ccc6ca3c75
Jannes Höke <mail@jhoeke.de>
2022-09-16 20:14:58 +0200

    🔥 Remove unused imports

--- src/colorMap.js
+++ src/colorMap.js
@@ -1,17 +1,17 @@
-import { scaleLinear } from 'd3-scale';
-import { rgb } from 'd3-color';
-import { interpolateRgb, interpolateHcl } from 'd3-interpolate';
-import { interpolateMagma, interpolateBlues } from 'd3-scale-chromatic'
+import { scaleLinear } from 'd3-scale'
+import { rgb } from 'd3-color'
+import { interpolateHcl } from 'd3-interpolate'
+import { interpolateMagma } from 'd3-scale-chromatic'
 
-
-export function colorMapMain(fac) {
-    const color = interpolateMagma(fac * 0.90 + 0.1)
-    return color
+export function colorMapMain (fac) {
+  const color = interpolateMagma(fac * 0.9 + 0.1)
+  return color
 }
-const interpolateAlt = scaleLinear().domain([0, 1])
-    .interpolate(interpolateHcl)
-    .range([rgb("#fff"), rgb('#00345f')]);
+const interpolateAlt = scaleLinear()
+  .domain([0, 1])
+  .interpolate(interpolateHcl)
+  .range([rgb('#fff'), rgb('#00345f')])
 
-export function colorMapAlt(fac) {
-    return interpolateAlt(fac)
+export function colorMapAlt (fac) {
+  return interpolateAlt(fac)
 }
